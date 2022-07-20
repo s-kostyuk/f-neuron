e583d93d6b5cc3d8dff41908ad8e3a724f7f4cf8
Serhii Kostiuk <contact@s-kostyuk.me>
2022-07-20 12:55:50 +0300

    Improved tests for Triangular membership functions
    - fixed improper usage of PyTorch equality checks;
    - fixed conversion of expected values to Tensors;
    - generalized test cases for different radius and center values;
    - removed dead/commented code.

--- tests/test_triangular.py
+++ tests/test_triangular.py
@@ -12,77 +12,76 @@ class TestTriangularMemberFunction(unittest.TestCase):
         self.function = TriangularMembF(self.radius, self.center)
 
     def test_forward_too_left(self):
-        in_ = torch.Tensor([-2.0])
+        in_ = torch.Tensor([self.center - self.radius * 2.0])
         out_ = self.function.forward(in_)
-        expected = 0.0
+        expected = torch.Tensor([0.0])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_left_edge(self):
-        in_ = torch.Tensor([-1.0])
+        in_ = torch.Tensor([self.center - self.radius])
         out_ = self.function.forward(in_)
-        expected = 0.0
+        expected = torch.Tensor([0.0])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_center(self):
         in_ = torch.Tensor([self.center])
         out_ = self.function.forward(in_)
-        expected = 1.0
+        expected = torch.Tensor([1.0])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_left(self):
-        #in_ = self.center - torch.rand(1) / self.radius
         in_ = torch.Tensor([self.center - self.radius / 2.0])
         out_ = self.function.forward(in_)
-        expected = 0.5
+        expected = torch.Tensor([0.5])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_left_rand(self):
-        in_ = self.center - torch.rand(1) / self.radius
+        in_ = self.center - torch.rand(1) * self.radius
         out_ = self.function.forward(in_)
-        expected = 1.0 - torch.absolute(in_)
+        expected = torch.Tensor([1.0 - torch.absolute(in_ - self.center) / self.radius])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_right_rand(self):
-        in_ = self.center + torch.rand(1) / self.radius
+        in_ = self.center + torch.rand(1) * self.radius
         out_ = self.function.forward(in_)
-        expected = 1.0 - torch.absolute(in_)
+        expected = torch.Tensor([1.0 - torch.absolute(in_ - self.center) / self.radius])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_right(self):
-        in_ = torch.Tensor([+0.5])
+        in_ = torch.Tensor([self.center + self.radius / 2.0])
         out_ = self.function.forward(in_)
-        expected = 0.5
+        expected = torch.Tensor([0.5])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_right_edge(self):
-        in_ = torch.Tensor([+1.0])
+        in_ = torch.Tensor([self.center + self.radius])
         out_ = self.function.forward(in_)
-        expected = 0.0
+        expected = torch.Tensor([0.0])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
     def test_forward_too_right(self):
-        in_ = torch.Tensor([+2.0])
+        in_ = torch.Tensor([self.center + self.radius * 2.0])
         out_ = self.function.forward(in_)
-        expected = 0.0
+        expected = torch.Tensor([0.0])
 
         self.assertTrue(isinstance(out_, torch.Tensor))
-        self.assertTrue(torch.eq(out_, expected))
+        self.assertTrue(torch.allclose(out_, expected))
 
 
 if __name__ == '__main__':
